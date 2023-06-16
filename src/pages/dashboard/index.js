d538c4b2142a29e1cd40e2581cd75343955abbde
Vitoria Belshoff <103432976+vitoriabelshoff@users.noreply.github.com>
2023-06-16 09:51:20 -0300

    Update index.js

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,1 +1,22 @@
+import { Button, Image, Text, View } from "react-native";
 
+
+import styles from './styles';
+
+
+export default function Filme(props){
+    return (
+        <View style={styles.container}>
+            <View style={styles.viewImg}>
+                <Image source={{
+                    uri: props.filme.imgUrl
+                }} style={styles.img}/>
+            </View>
+            <View style={styles.viewData}>
+                <Text style={styles.Item}>Item: {props.filme.Item}</Text>
+                <Text style={styles.Descrição}>Descrição: {props.filme.Descrição}</Text>
+                <Button style={styles.btn}color='green' title="Trocar"/>
+            </View>
+        </View>
+    );
+}
