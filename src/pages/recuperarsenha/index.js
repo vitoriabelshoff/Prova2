b56fc2ed7797ddd9605d4a49e6551d63838a5668
Vitoria Belshoff <103432976+vitoriabelshoff@users.noreply.github.com>
2023-06-16 10:01:05 -0300

    Update index.js

--- src/pages/recuperarsenha/index.js
+++ src/pages/recuperarsenha/index.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import { View, TextInput, Button } from 'react-native';
 
+
+export default function RecuperarSenha() {
+  const handleRecuperarSenha = () => {
+    // Lógica para recuperar a senha
+  };
+
+
+  return (
+    <View>
+      <TextInput placeholder="Email" />
+      <Button title="Recuperar Senha" onPress={handleRecuperarSenha} />
+    </View>
+  );
+}
