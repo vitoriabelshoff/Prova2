fee17919c55c6dc42cef1fbf5eabc2d08b9b8f7f
Vitoria Belshoff <103432976+vitoriabelshoff@users.noreply.github.com>
2023-06-16 09:49:29 -0300

    Update index.js

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -1,1 +1,24 @@
+import React from 'react';
+import { View, TextInput, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
+
+export default function Cadastro() {
+  const navigation = useNavigation();
+
+
+  const handleLoginPress = () => {
+    navigation.navigate('Login');
+  };
+
+
+  return (
+    <View>
+      <TextInput placeholder="Nome" />
+      <TextInput placeholder="Email" />
+      <TextInput placeholder="Senha" secureTextEntry={true} />
+      <Button title="Cadastrar" onPress={() => {}} />
+      <Button title="Já possui uma conta? Faça login" onPress={handleLoginPress} />
+    </View>
+  );
+}
