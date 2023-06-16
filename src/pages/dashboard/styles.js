dfedcaf71b8de61b945944d231d9913532447346
Vitoria Belshoff <103432976+vitoriabelshoff@users.noreply.github.com>
2023-06-16 09:51:44 -0300

    Update styles.js

--- src/pages/dashboard/styles.js
+++ src/pages/dashboard/styles.js
@@ -1,1 +1,48 @@
+import { StyleSheet } from "react-native";
 
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row',
+        padding: 10,
+        margin: 3,
+        alignItems: 'center',
+        justifyContent: 'flex-start'
+    },
+    viewImg: {
+        width: '30%',
+        height: 150,
+        backgroundColor: 'black'
+    },
+    img: {
+        width: '100%',
+        height: '100%',
+        borderRadius: 10
+    },
+    viewData: {
+        backgroundColor: 'grey',
+        padding: 10,
+        width: '70%',
+        height: '100%',
+    },
+    Item: {
+        fontSize: 18,
+        color: 'white',
+        fontWeight: 'bold',
+        marginBottom: 5
+    },
+    Descrição: {
+        fontSize: 14,
+        color: 'white',
+        marginBottom: 5
+    },
+   
+    btn: {
+        padding: 10,
+        backgroundColor: 'white'
+    }
+});
+
+
+export default styles;
