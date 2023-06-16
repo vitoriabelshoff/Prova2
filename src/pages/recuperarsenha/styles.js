8c0d8d400d6752ce31cd40816791607ce8adbdf2
Vitoria Belshoff <103432976+vitoriabelshoff@users.noreply.github.com>
2023-06-16 10:02:52 -0300

    Update styles.js

--- src/pages/recuperarsenha/styles.js
+++ src/pages/recuperarsenha/styles.js
@@ -1,1 +1,21 @@
+import { StyleSheet } from 'react-native';
 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+  },
+});
+
+
+export default styles;
