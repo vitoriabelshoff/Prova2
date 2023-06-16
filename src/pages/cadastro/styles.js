46abd26d312e8528c0c0865f6e654c76886a56ca
Vitoria Belshoff <103432976+vitoriabelshoff@users.noreply.github.com>
2023-06-16 09:50:14 -0300

    Update styles.js

--- src/pages/cadastro/styles.js
+++ src/pages/cadastro/styles.js
@@ -1,1 +1,34 @@
+import { StyleSheet } from 'react-native';
 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginBottom: 10,
+    width: '80%',
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    width: '80%',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  loginButton: {
+    marginTop: 20,
+  },
+});
+
+
+export default styles;
