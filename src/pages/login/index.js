bc27252c73bdcb83afb009a4ae36a26910700d33
Vitoria Belshoff <103432976+vitoriabelshoff@users.noreply.github.com>
2023-06-16 09:55:10 -0300

    Update index.js

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import { View, TextInput, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
+
+export default function Login() {
+  const navigation = useNavigation();
+
+
+  const handleCadastroPress = () => {
+    navigation.navigate('Cadastro');
+  };
+
+
+  return (
+    <View>
+      <TextInput placeholder="Email" />
+      <TextInput placeholder="Senha" secureTextEntry={true} />
+      <Button title="Entrar" onPress={() => {}} />
+      <Button title="Cadastrar" onPress={handleCadastroPress} />
+    </View>
+  );
+}
